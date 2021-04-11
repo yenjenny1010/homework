30c97f33c77acfab70a280b84bf59b2d704212db
yenjenny1010 <69136272+yenjenny1010@users.noreply.github.com>
2021-04-11 10:59:42 +0800

    6.py finish

--- EX06_08.py
+++ EX06_08.py
@@ -1,19 +1,12 @@
-
 def celsiusToFahrenheit(celsius):
-    return (9.0 / 5.0) * celsius + 32
-
-def fahrenheitToCelsius(fahrenheit):
-    return (5.0 / 9) * (fahrenheit - 32)
-
-print(format("Celsius", "<15s"), format("Fahrenheit", "<15s"), "  |    ", format("Fahrenhei", "<15s"), format("Celsius", "<15s")) 
-print("---------------------------------------------------------------")
-
-celsius = 40
-farenheit = 120
-i = 1
-    
-while i <= 10:
-    print(format(celsius, "<15d"), format(celsiusToFahrenheit(celsius), "<15.2f"), "  |    ", format(farenheit, "<15d"), format(fahrenheitToCelsius(farenheit), "<15.2f"))
-    celsius -= 1
-    farenheit -= 10
-    i += 1
+    Fahrenheit=(9/5)*celsius+32
+    return Fahrenheit
+def FahrenheitTocelsius(Fahrenheit):
+    celsius=(5/9)*(Fahrenheit-32)
+    return celsius
+print(format("Celsius", "<16s"),format("Fahrenheit", "<16s"),"|","     ",format("Fahrenheit", "<16s"),"Celsius")
+for i in range(0,10):
+    Fahrenheit=celsiusToFahrenheit(40-i)
+    celsius=FahrenheitTocelsius(120-i*10)
+    print(format("%.2f"%(40-i),"<16s"),format("%.2f"%Fahrenheit,"<16s"),"|","     ",format("%.2f"%(120-i*10),"<16s"),"%.2f" %celsius)
+    format("Celsius", "<15s")
